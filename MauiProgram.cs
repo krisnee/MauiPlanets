e4e1e8e22b3617650b7656e8c6e485199067f41f
Kris Nee <kristi.neemesto@gmail.com>
2025-02-24 21:52:20 +0200

    Colors and styles

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,9 +11,10 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("Montserrat-Medium.ttf", "RegularFont");
+                fonts.AddFont("Montserrat-SemiBold.ttf", "MediumFont");
+                fonts.AddFont("Montserrat-Bold.ttf", "BoldFont");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
